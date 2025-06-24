d8a7fcc1f8e7eacdf487304284a95397df761d42
SilentisVox <pdnvzjivvmum@gmail.com>
2025-06-24 19:20:57 -0400

    Minor changes

--- Silence.py
+++ Silence.py
@@ -12,37 +12,28 @@ def main() -> None:
 
         parser                          = argparse.ArgumentParser()
         parser.add_argument("-c", type=str, help="Callback address")
-        parser.add_argument("-p", type=str, help="TCP Handler port (Default 4443)")
-        parser.add_argument("-l", type=str, help="HTTP Stager port (Default 8080)")
+        parser.add_argument("-p", type=int, help="TCP Handler port (Default 4443)", default=4443)
+        parser.add_argument("-l", type=int, help="HTTP Stager port (Default 8080)", default=8080)
+        parser.add_argument("-d", type=int, help="Client dwell time.", default=2)
         args                            = parser.parse_args()
 
         if not args.c:
                 core.TextAssets.print_debug()
                 return
 
-        if not args.l:
-                args.l                  = 4443
-
-        if not args.p:
-                args.p                  = 8080
-
         core.TextAssets.print_info("Starting services")
         core.TextAssets.print_info("Welcome to the silence shell!") 
         core.TextAssets.print_info("Please type 'help' for more options")
 
-        silent_server                   = SilentServer(args.c, args.l)
+        silent_server                   = SilentServer(args.c, args.p, args.d)
         silent_server.initialize_server()
         
-        http_server                     = HTTPServer(args.c, args.p, args.l)
+        http_server                     = HTTPServer(args.c, args.l, args.p)
         http_server.initialize_server()
 
-        core.TextAssets.print_services(args.l, args.p)
+        core.TextAssets.print_services(args.p, args.l)
 
-        command_handler                 = CommandHandler(
-                silent_server,
-                http_server,
-        )
-
+        command_handler                 = CommandHandler(silent_server, http_server)
         command_handler.generate()
 
         try:
